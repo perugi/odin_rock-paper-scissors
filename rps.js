d76eb0a0b070636623f2d22fb414b25232d8f81e
perugi <dominik.perusko@gmail.com>
2023-02-23 23:33:26 +0100

    Implement functionality with playing the game with GUI

--- rps.js
+++ rps.js
@@ -1,3 +1,22 @@
+let playerScore = 0;
+let computerScore = 0;
+let roundNo = 1;
+
+const shapeBtns = document.querySelectorAll(".shape");
+shapeBtns.forEach(shapeBtn => shapeBtn.addEventListener('click', playRound));
+
+// Plays a single round of rock-paper-scissors.
+function playRound(e) {
+    playerSelection = e.target.id;
+    computerSelection = getComputerChoice();
+    const winner = getWinner(playerSelection, computerSelection);
+
+    updateInfo(winner, playerSelection, computerSelection);
+
+    if (playerScore === 5 || computerScore === 5) endGame(winner);
+}
+
+
 // Randomly returns 'rock', 'paper' or 'scissors'.
 function getComputerChoice() {
     shapes = ['rock', 'paper', 'scissors'];
@@ -5,8 +24,9 @@ function getComputerChoice() {
     return shapes[Math.floor(Math.random() * shapes.length)];
 }
 
-// Plays a single round of rock-paper-scissors.
-function playRound(playerSelection, computerSelection) {
+
+// Based on rock paper scissor rules and the selection parameters, return the winner of the round.
+function getWinner(playerSelection, computerSelection) {
     // Convert player selection to lower case to avoid input errors.
     playerSelection = playerSelection.toLowerCase();
 
@@ -34,39 +54,102 @@ function playRound(playerSelection, computerSelection) {
         }
     }
 
-    // Return the appropriate string.
-    switch (winner) {
-        case 'draw':
-            return 'The round is a draw.';
-        case 'player':
-            return `You win! ${toTitleCase(playerSelection)} beats ${computerSelection}.`;
-        case 'computer':
-            return `You lose! ${toTitleCase(computerSelection)} beats ${playerSelection}.`;
-        default:
-            throw new Error('Round results are invalid (not player, computer or draw.');
-    }
+    return winner
 }
 
+
+// After a round winner has been determined, update the info in the DOM.
+function updateInfo(winner, playerSelection, computerSelection) {
+    roundInfo = document.querySelector("#round-info");
+    playerScoreDisplay = document.querySelector("#player-score");
+    computerScoreDisplay = document.querySelector("#computer-score");
+    switch (winner) {
+        case 'player':
+            roundInfo.textContent = `You win! ${toTitleCase(playerSelection)} `;
+            if (playerSelection === "scissors") roundInfo.textContent += "beat ";
+            else roundInfo.textContent += "beats ";
+            roundInfo.textContent += `${computerSelection}.`;
+
+            playerScoreDisplay.textContent = ++playerScore;
+            break;
+        case 'computer':
+            roundInfo.textContent = `You lose. ${toTitleCase(computerSelection)} `;
+            if (computerSelection === "scissors") roundInfo.textContent += "beat ";
+            else roundInfo.textContent += "beats ";
+            roundInfo.textContent += `${playerSelection}.`;
+
+            computerScoreDisplay.textContent = ++computerScore;
+            break;
+        case 'draw':
+            roundInfo.textContent = "It's a draw."
+            break;
+        case 'default':
+            throw new Error('Invalid response received from getWinner function.')
+    }
+
+    gameInfo = document.querySelector("#game-info");
+    gameInfo.textContent = `Round ${++roundNo} - Select your weapon:`;
+
+    playerLog = document.querySelector("#player-logs");
+    newPlayerLog = document.createElement("li");
+    newPlayerLog.textContent = toTitleCase(playerSelection);
+    playerLog.appendChild(newPlayerLog);
+
+    computerLog = document.querySelector("#computer-logs");
+    newComputerLog = document.createElement("li");
+    newComputerLog.textContent = toTitleCase(computerSelection);
+    computerLog.appendChild(newComputerLog);
+}
+
+
+// Helper function, returning the passed string in Title Case (capitalizes only the first 
+// character of the first word in case multiple are contained in the string).
 function toTitleCase(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
-// Plays the game 5 times by getting the user and computer input and printing out the results.
-function game() {
-    // Define the valid shapes for checking the user input
-    validShapes = ['rock', 'paper', 'scissors'];
 
-    for (let i = 0; i < 5; i++) {
-        // Ask the user for input until he inputs a valid shape
-        let playerSelection = "";
-        while (!validShapes.includes(playerSelection.toLowerCase())) {
-            playerSelection = prompt("Input your selection [rock, paper, scissors]:");
-        }
+// Display the information at the end of the game and allow the user to press the spacebard for a new game.
+function endGame(winner) {
 
-        console.log(playRound(playerSelection, getComputerChoice()));
-    }
+    gameInfo = document.querySelector("#game-info")
+    roundInfo = document.querySelector("#round-info");
 
-    console.log("Game over, thanks for playing!")
+    gameInfo.textContent = "Game over! "
+    if (winner === "player") gameInfo.textContent += "You win!"
+    else gameInfo.textContent += "The computer wins!"
+
+    roundInfo.textContent = "Press spacebar to play another game."
+
+    shapeBtns.forEach(btn => btn.disabled = true);
+    document.addEventListener("keydown", newGame);
 }
 
-game();
+// If spacebar was pressed, reset the game start and update the DOM to show this.
+function newGame(e) {
+    if (e.keyCode != 32) return;
+
+    playerScore = 0;
+    computerScore = 0;
+    roundNo = 1;
+
+    gameInfo = document.querySelector("#game-info");
+    gameInfo.textContent = `Round 1 - Select your weapon:`;
+    ;
+
+    shapeBtns.forEach(btn => btn.disabled = false);
+
+    roundInfo = document.querySelector("#round-info");
+    roundInfo.innerHTML = "&nbsp;"
+
+    playerScoreDisplay = document.querySelector("#player-score");
+    computerScoreDisplay = document.querySelector("#computer-score");
+    playerScoreDisplay.textContent = "0";
+    computerScoreDisplay.textContent = "0";
+
+    playerLogs = document.querySelectorAll("#player-logs > li")
+    playerLogs.forEach(playerLog => playerLog.remove())
+
+    computerLogs = document.querySelectorAll("#computer-logs > li")
+    computerLogs.forEach(computerLog => computerLog.remove())
+}
